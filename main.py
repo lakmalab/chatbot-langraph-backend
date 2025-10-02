6ef56eea2b01ea495cbd6ed176b4cae72aef1419
lakma <lakmalaberathne@gmail.com>
2025-10-02 10:52:15 +0530

    feat: setup config.py and .env , added pymysql

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from core.config import settings
 
 app = FastAPI(
     title= "ChatBot Assistant Service",
@@ -10,7 +11,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=settings.ALLOWED_ORIGINS,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"]
