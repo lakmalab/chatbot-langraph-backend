9c0bdf0012d725342a44cc54231d09ff4490d659
lakma <lakmalaberathne@gmail.com>
2025-10-07 18:50:53 +0530

    fix : fix imports

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,3 +1,7 @@
+from app.models.chat_message import ChatMessage
+from app.models.conversation import Conversation
+from app.models.pension_calculation import PensionCalculation
+from app.models.scheme import Scheme
 from app.models.session import Session
 
 __all__ = [
--- app/schemas/__init__.py
+++ app/schemas/__init__.py
@@ -1,3 +1,4 @@
+from app.models import Scheme, Conversation, ChatMessage, PensionCalculation
 from app.models.session import Session
 
 __all__ = [
